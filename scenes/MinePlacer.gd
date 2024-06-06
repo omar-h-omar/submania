5bfbedc85eb3b8199a5a635b4e0b6b6627d99aff
Adrian <81231786+adrian-lote@users.noreply.github.com>
2024-06-06 20:45:01 +0100

    Restart and mine spawner

--- scenes/MinePlacer.gd
+++ scenes/MinePlacer.gd
@@ -4,23 +4,33 @@ extends Node
 @export var SpawnBoundsMin : Vector2
 @export var SpawnBoundsMax : Vector2
 @export var MinesToSpawn := 5
-@export var MinDistanceBetween := 1.0
+@export var MinDistanceBetween := 10.0
 
 var _spawnPositions : Array
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in MinesToSpawn:
-		var xPos = randf_range(SpawnBoundsMin.x, SpawnBoundsMax.x)
-		var yPos = randf_range(SpawnBoundsMin.y, SpawnBoundsMax.y)
+		var spawnPos = get_new_spawn_pos()
+				
 		
 		var mine = MinePrefab.instantiate()
-		mine.global_position = Vector2(xPos, yPos)
+
+		mine.global_position = spawnPos
+		
+		_spawnPositions.push_back(spawnPos)
 		
 		add_child(mine)
 		
 		
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func get_new_spawn_pos():
+	var xPos = randf_range(SpawnBoundsMin.x, SpawnBoundsMax.x)
+	var yPos = randf_range(SpawnBoundsMin.y, SpawnBoundsMax.y)
+	var spawnPos = Vector2(xPos, yPos)
+	
+	for pos in _spawnPositions:
+		if pos.distance_to(spawnPos) < MinDistanceBetween:
+			print("re-roll")
+			spawnPos = get_new_spawn_pos()
+			
+	return spawnPos
