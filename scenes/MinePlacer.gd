9fc6248bec32e06d3332c0e0820979f2f41182c3
Adrian <81231786+adrian-lote@users.noreply.github.com>
2024-06-06 20:24:52 +0100

    placer update

--- scenes/MinePlacer.gd
+++ scenes/MinePlacer.gd
@@ -1,12 +1,21 @@
 extends Node
 
 @export var MinePrefab : PackedScene
-@export var SpawnBounds : Vector2
-
+@export var SpawnBoundsMin : Vector2
+@export var SpawnBoundsMax : Vector2
+@export var MinesToSpawn := 5
+@export var MinDistanceBetween := 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for i in MinesToSpawn:
+		var xPos = randf_range(SpawnBoundsMin.x, SpawnBoundsMax.x)
+		var yPos = randf_range(SpawnBoundsMin.y, SpawnBoundsMax.y)
+		
+		var mine = MinePrefab.instantiate()
+		mine.global_position = Vector2(xPos, yPos)
+		
+		add_child(mine)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
