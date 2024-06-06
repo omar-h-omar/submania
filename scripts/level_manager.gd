06d1d3f0537089f6ae23ba4b0ab2d44a57057772
Omar Omar <omarhatem5555@gmail.com>
2024-06-06 20:09:09 +0100

    adding level manager

---
+++ scripts/level_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var current_scene = null
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var root = get_tree().root
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func next_level():
+	current_scene.queue_free()
+	var nextLevel = ResourceLoader.load("res://scenes/game.tscn")
+	current_scene = nextLevel.instantiate()
+	get_tree().root.add_child(current_scene)
+	get_tree().current_scene = current_scene
+
